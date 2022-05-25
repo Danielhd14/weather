e3b834b6b78979bf9f250eb66006fcb450071aa3
harry.daniel <danielharry353@gmail.com>
2022-05-25 15:14:54 +0100

    fix

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ const option = {
     }
 }
 
-
+let hold = document.getElementById('hold')
 async function getWeather(city) {
     const response = await fetch(url + city + "&units=metric&appid=" + apiKey, option);
     const data = await response.json();
     console.log(data)
-    if(document.body.childElementCount < 2){
-        console.log(document.body.childElementCount)
-    document.body.innerHTML += `
+    hold.innerHTML = `
     <div class="info" id="info">
     <div class="headlines">
     <h1>Weather in ${data.name} at ${data.sys.country}</h1>
@@ -29,22 +27,6 @@ async function getWeather(city) {
 </div>
 </div>
     `
-    }   else{
-        let info = document.getElementById('info')
-        info.innerHTML = `
-        <div class="headlines">
-        <h1>Weather in ${data.name} at ${data.sys.country}</h1>
-        <h2>At coordinate ${data.coord.lat} and ${data.coord.lon}</h2>   
-    </div>
-    <div class="headlines">
-        <h3>It is ${data.main.temp} Degrees C</h3>
-    </div>
-    <div class="headlines">
-        <h3>With Wind speed of ${data.wind.speed}</h3>
-        <h3>Looks like we are going to have ${data.weather[0].description}</h3>
-    </div>
-        `
-        }
 }
 
 
